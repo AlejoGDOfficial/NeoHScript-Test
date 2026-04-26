29fe44014eb1f9274ae8100de1fe80f38f386f58
AlejoGDOfficial <alejogdoff@gmail.com>
2026-04-26 09:53:44 -0500

    Linux doesn't sort files alphabetically :sob:

--- source/Main.hx
+++ source/Main.hx
@@ -12,6 +12,8 @@ class Main
 
         final files:Array<String> = FileSystem.readDirectory('scripts').filter(file -> file.endsWith(ScriptsConfig.EXTENSION));
 
+        files.sort(Reflect.compare);
+
         for (index => file in files)
         {
             new HScript(file.substr(0, file.length - ScriptsConfig.EXTENSION.length), true);
